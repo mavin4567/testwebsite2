de9f9100c1ed0c94338abbb922cb968e21c665c4
mavin4567 <mavin456@gmail.com>
2026-06-11 15:05:05 +0700

    Delete app directory

--- app/layout.tsx
+++
@@ -1,4 +0,0 @@
-
-export default function RootLayout({children}:{children:React.ReactNode}){
-return <html lang="th"><body style={{margin:0,fontFamily:'Inter,Arial,sans-serif'}}>{children}</body></html>
-}
